16e71201b58b9d43ba6dfaae524cd11ce7a7cca9
aasthashrimal <aasthashrimal22@gmail.com>
2025-08-18 23:44:54 +0530

    Update Approach1.java

--- #242. Valid Anagram/Approach1.java
+++ #242. Valid Anagram/Approach1.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+            return false;
+        char c[] = s.toCharArray();
+        char d[] = t.toCharArray();
+        Arrays.sort(c);
+        Arrays.sort(d);
+        s = new String(c);
+        t = new String(d);
+        return s.equals(t);
+    }
+}
